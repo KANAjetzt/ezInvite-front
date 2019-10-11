6f4f29b9ef1d2e9700d96f5cc64293fcce5c9e25
Kai Roth <kai@kana.jetzt>
2019-10-11 12:34:04 +0200

    HF HF - spend some time thingking about SVGs 🤔

---
+++ src/components/Hero.svelte
@@ -0,0 +1,28 @@
+<script>
+  import CalendarIcon from "./Icons/Calendar.svelte";
+  import LocationPinIcon from "./Icons/LocationPin.svelte";
+</script>
+
+<style>
+  h1,
+  p {
+    font-family: var(--font-primary);
+    color: #eee;
+  }
+</style>
+
+<header class="hero">
+  <div class="title">
+    <h1>city grill</h1>
+  </div>
+  <div class="box">
+    
+  </svg>
+
+    <p>12.12.2019</p>
+  </div>
+  <div class="box">
+    <LocationPinIcon />
+    <p>Konstanz</p>
+  </div>
+</header>
