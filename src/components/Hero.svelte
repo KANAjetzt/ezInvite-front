1bc239c2285557acdce798a07444c392c496d9b1
Kai Roth <kai@kana.jetzt>
2019-10-14 09:56:51 +0200

    Hero Component with img Tag

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,28 +1,102 @@
 <script>
   import CalendarIcon from "./Icons/Calendar.svelte";
   import LocationPinIcon from "./Icons/LocationPin.svelte";
+
+  let bgImage =
+    "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
 </script>
 
 <style>
+  .hero {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding-bottom: 10rem;
+    background-image: linear-gradient(
+      89.87deg,
+      rgba(4, 120, 208, 0.8) -2.53%,
+      rgba(4, 125, 217, 0.8) 112.27%
+    );
+    clip-path: polygon(0 0, 100% 0, 100% 85%, 0% 100%);
+  }
+  .topBar {
+    width: 100%;
+    height: 0.5rem;
+    margin-bottom: 4rem;
+    background-image: linear-gradient(
+      89.87deg,
+      #0476d0 -2.53%,
+      #047dd9 112.27%
+    );
+  }
+
+  .bgImg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: -1;
+  }
+
   h1,
   p {
     font-family: var(--font-primary);
-    color: #eee;
+    font-weight: 400;
+    color: var(--color-text-primary);
+    margin: 0;
+  }
+
+  h1 {
+    text-transform: uppercase;
+    text-align: center;
+    padding: 1rem 4rem;
+  }
+
+  .title {
+    align-self: center;
+    background-image: linear-gradient(
+        180deg,
+        rgba(255, 255, 255, 0.2) 0%,
+        rgba(255, 255, 255, 0) 100%
+      ),
+      linear-gradient(
+        180deg,
+        rgba(4, 118, 208, 0) 0%,
+        rgba(4, 118, 208, 0.5) 100%
+      );
+  }
+
+  p {
+    font-size: 1.4rem;
+    margin-left: 0.5rem;
+  }
+
+  .info {
+    margin-top: 2rem;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .box {
+    display: flex;
+    align-items: center;
   }
 </style>
 
 <header class="hero">
+  <div class="topBar" />
   <div class="title">
     <h1>city grill</h1>
   </div>
-  <div class="box">
-    
-  </svg>
-
-    <p>12.12.2019</p>
+  <div class="info">
+    <div class="box">
+      <CalendarIcon width={16} height={16} fill={'#f9fafb'} />
+      <p>12.12.2019</p>
+    </div>
+    <div class="box">
+      <LocationPinIcon width={16} height={16} fill={'#f9fafb'} />
+      <p>Konstanz</p>
+    </div>
   </div>
-  <div class="box">
-    <LocationPinIcon />
-    <p>Konstanz</p>
-  </div>
+  <img src={bgImage} alt="Will get this from API later!" class="bgImg" />
 </header>
