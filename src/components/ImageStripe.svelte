4a2c907bcefc3bac3048373c7db44bcc82895c0d
Kai Roth <kai@kana.jetzt>
2019-10-16 15:31:17 +0200

    Polished ImageStripe - ParentBox height calc

--- src/components/ImageStripe.svelte
+++ src/components/ImageStripe.svelte
@@ -1,23 +1,43 @@
 <script>
-  //TODO: Add some JS to calculate the height vor the Image Stripe.
+  //TODO: Maybe add something to adjust heigh of box when Browser size changes
+  import { beforeUpdate, afterUpdate } from "svelte";
 
   import { getEventData } from "../stores";
 
   let eventData = getEventData();
-  console.log(eventData);
+  let imageStripeHeight;
+  let ready = false;
+
+  // Sets the height of the Image Stripe parent Box
+  // ready is used to animate the Image Stripe in when the with is set
+  // The Timeout is required - images need to load bevor the height can be acquired
+  afterUpdate(() => {
+    setTimeout(() => {
+      imageStripeHeight = document
+        .querySelector(".imageStripe")
+        .getBoundingClientRect().height;
+      console.log(imageStripeHeight);
+      ready = true;
+    }, 10);
+  });
 </script>
 
 <style>
   .box {
     display: flex;
     align-items: center;
-    height: 30vh;
     overflow: hidden;
+    opacity: 0;
+    transition: all 0.3s ease-in;
   }
+
   .imageStripe {
     display: grid;
     grid-template-columns: 1fr 1fr;
     transform: rotate(-9deg) scale(1.2);
+    box-shadow: -8px -5px 7px 0px hsl(206, 95%, 15%), -4px -5px 0px 0px #000,
+      8px 4px 11px 0px hsl(206, 95%, 15%), inset -13px -20px 0px 0px #000,
+      2px 4px 0px 0px #000;
   }
   .img {
     width: 100%;
@@ -26,7 +46,7 @@
   }
 </style>
 
-<div class="box">
+<div class="box" style="height:{imageStripeHeight}px; opacity:{ready ? 1 : 0}">
   <section class="imageStripe">
     {#each eventData.imgs as img}
       <div class="imgBox">
